5dac51e605bb88e90c754ea62601884ea6a7431e
guowei <1435257523@qq.com>
2019-11-08 19:58:19 +0800

    init

---
+++ src/pages/Msite/Msite.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Msite
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus">
+
+</style>
+
---
+++ src/pages/Search/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Search
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus">
+
+</style>
+
